f1eddb4df27cd60bfafed48253f2ca25b4a8ed74
Corso <corsodix@gmail.com>
2016-08-05 21:32:04 +0400

    добавлены линии

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,18 +10,21 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
+            Point p1 = new Point(1,3,'*');
             p1.Draw();
 
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
+            Point p2 = new Point(4,5,'#');
             p2.Draw();
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+' );
+            line.DrawLine();
+
+            VerticalLine vLine = new VerticalLine(8, 15, 10,'+');
+            vLine.DrawLine();
 
             Console.ReadKey();
         }
