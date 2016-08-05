613ecb0f84ecd75984685c6cc49ab25fd201f30a
Corso <corsodix@gmail.com>
2016-08-05 21:48:06 +0400

    class & parent class

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,21 +10,36 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1,3,'*');
-            p1.Draw();
+            var x = Console.WindowWidth;
+            //Console.SetBufferSize(80, 25);
 
-            Point p2 = new Point(4,5,'#');
-            p2.Draw();
 
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
 
-            HorizontalLine line = new HorizontalLine(5, 10, 8, '+' );
-            line.DrawLine();
+            upLine.DrawLine();
+            downLine.DrawLine();
+            leftLine.DrawLine();
+            rightLine.DrawLine();
 
-            VerticalLine vLine = new VerticalLine(8, 15, 10,'+');
-            vLine.DrawLine();
+
+            //Point p1 = new Point(1,3,'*');
+            //p1.Draw();
+
+            //Point p2 = new Point(4,5,'#');
+            //p2.Draw();
+
+            //List<Point> pList = new List<Point>();
+            //pList.Add(p1);
+            //pList.Add(p2);
+
+            //HorizontalLine line = new HorizontalLine(5, 10, 8, '+' );
+            //line.DrawLine();
+
+            //VerticalLine vLine = new VerticalLine(8, 15, 10,'+');
+            //vLine.DrawLine();
 
             Console.ReadKey();
         }
