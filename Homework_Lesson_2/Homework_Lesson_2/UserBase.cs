6f779a30750a6758515d4cdf67945406666f45bf
Corso <corsodix@gmail.com>
2016-08-07 14:10:50 +0400

    второй урок

---
+++ Homework_Lesson_2/Homework_Lesson_2/UserBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Lesson_2
+{
+    class UserBase
+    {
+        public enum userBase : int
+        {
+            Admin = 1,
+            User = 4,
+            Guest = 7
+        }
+        public string login, password;
+        public void LoginInput()
+        {
+            Console.Write("Введите ваш логин: ");
+            login = Console.ReadLine();
+            Console.Write("Введите ваш пароль: ");
+            password = Console.ReadLine();
+        }
+
+    }
+}
