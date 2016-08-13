d4797b435c4c569ca812bd17a0485f8ed1b8d770
Corso <corsodix@gmail.com>
2016-08-13 17:38:38 +0400

    завершенное ДЗ№3

--- HomeWork_Lesson_3/HomeWork_Lesson_3/Program.cs
+++ HomeWork_Lesson_3/HomeWork_Lesson_3/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
 
 namespace HomeWork_Lesson_3
 {
@@ -10,8 +12,116 @@ namespace HomeWork_Lesson_3
     {
         static void Main(string[] args)
         {
+            string x="";
+            while(x!="q")
+            {
+                Console.Clear();
+                Console.WriteLine("Главное меню");
+                Console.WriteLine("1 - для запуска калькулятора комплексных чисел\n2 - для запуска калькулятора дробей\n3 - для работы с фигурами");
+                Console.WriteLine("Введите число: ");
+                x = Console.ReadLine();
+                switch(x)
+                {
+                    case "1":
+                        ComplexCalc();
+                        break;
+                    case "2":
+                        FractionsCalc();
+                        break;
+                    case "3":
+                        Shapes();
+                        break;
+                    default:
+                        continue;
+                }
+            }
+
+
+
+
+
             //ComplexCalc();
             //FractionsCalc();
+            //Shape.Position coord = new Shape.Position();
+            //coord.x = 10;
+            //coord.y = 10;
+            //Circle cir = new Circle(coord, 2);
+            //coord.x = 15;
+            //coord.y = 10;
+            //Square sq = new Square(coord, 3);
+            //coord.x = 10;
+            //coord.y = 15;
+            //Triangle tri = new Triangle(coord, 7, 5, 90);
+            //IDrawable draw = new Drawable();
+
+
+
+            //draw.DrawCircle(cir);
+            //draw.DrawSquare(sq);
+            //draw.DrawTriangle(tri);
+
+            //IPrintable print = new Printable();
+
+            //print.DrawSquare(sq);
+            //print.DrawTriangle(tri);
+
+
+            //Shape.Position triangleCoord = new Shape.Position();
+            //triangleCoord.x = 10;
+            //triangleCoord.y = 10;
+            //Shape tr = new Triangle(triangleCoord,5,5,90);
+
+            //var s = tr.Area();
+
+            //Console.WriteLine(tr.ToString());
+            //Console.WriteLine(s);
+
+            Console.ReadKey();
+
+        }
+        public static void Shapes()
+        {
+            Console.Clear();
+            IDrawable draw = new Drawable();
+
+            Shape.Position cirCoord = new Shape.Position();
+            cirCoord.x = 4;
+            cirCoord.y = 4;
+            Circle cir = new Circle(cirCoord, 2);
+            Console.SetCursorPosition(1, 1);
+            Console.WriteLine("1.Фигура {0}, с радиусом {1} и площадью {2}", cir.ToString(), cir.radius, cir.Area());
+            draw.DrawCircle(cir);
+            Shape.Position sqCoord = new Shape.Position();
+            sqCoord.x = 2;
+            sqCoord.y = 9;
+            Square sq = new Square(sqCoord, 3);
+            Console.SetCursorPosition(1, 8);
+            Console.WriteLine("2.Фигура {0}, с длинной стороны {1} и площадью {2}", sq.ToString(), sq.edgeLength, sq.Area());
+            draw.DrawSquare(sq);
+
+            Shape.Position triCoord = new Shape.Position();
+            triCoord.x = 2;
+            triCoord.y = 14;
+            Triangle tri = new Triangle(triCoord, 7, 5, 45);
+            Console.SetCursorPosition(1, 13);
+            Console.WriteLine("3.Фигура {0}, с длинной стороны одной стороны {1}, длиной другой стороны {2}, уголом между ними {3} и площадью {4}", tri.ToString(), tri.edge1, tri.edge2, tri.angle, sq.Area());
+            draw.DrawTriangle(tri);
+            Console.SetCursorPosition(1, 19);
+            Console.WriteLine("Какую фигуру вы хотите распечатать?");
+            string str = Console.ReadLine();
+            IPrintable print = new Printable();
+            switch (str)
+            {
+                case "1":
+                    print.DrawCircle(cir);
+                    break;
+                case "2":
+                    print.DrawSquare(sq);
+                    break;
+                case "3":
+                    print.DrawTriangle(tri);
+                    break;
+            }
         }
         public static void ComplexCalc()
         {
--- HomeWork_Lesson_3/HomeWork_Lesson_3/Shape.cs
+++ HomeWork_Lesson_3/HomeWork_Lesson_3/Shape.cs
@@ -6,14 +6,93 @@ using System.Threading.Tasks;
 
 namespace HomeWork_Lesson_3
 {
+    //4. * Реализовать базовый класс Shape и описать общие методы для работы с формами.Реализовать класс наследник от Shape: Triangle, Square, Circle.
+    //    Каждый их этих классов должен переопределять метод вычисления площади.Должен быть переопределен метод ToString для всех классов(включая Shape),
+    //    который должен выводить название текущего класса.Для треугольника так же можно попробовать реализовать наследников (Равнобедренный треугольник, 
+    //    прямоугольный треугольник). Типы фигур можно задавать через enum.
+
     class Shape
     {
-        int side, radius;
-        int corner;
-
-        public string ToString()
+        public int edgeLength, edgeCount, radius;
+        int s;
+        public Position position;
+        public string name;
+        public struct Position
         {
-            return "Shape";
+            public int x, y;
+            public Position(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+        public Shape(Position position, string name)
+        {
+            this.name = name;
+            this.position = position;
+        }
+        public virtual double Area()
+        {
+            return s;
+        }
+        public new string ToString()
+        {
+            return name;
         }
     }
+    class Square : Shape
+    {
+        public Square(Position position, int edgeLength) : base (position, "Квадрат")
+        {
+            this.edgeLength = edgeLength;
+            edgeCount = 4;
+        }
+        public override double Area()
+        {
+            return edgeLength*edgeLength; 
+        }
+        public new string ToString()
+        {
+            return name;
+        }
+    }
+    class Circle : Shape
+    {
+        public Circle(Position position, int radius) : base(position, "Круг")
+        {
+            this.radius = radius;
+        }
+        public override double Area()
+        {
+            return Math.PI*Math.Sqrt(Convert.ToDouble(radius));
+        }
+
+        public new string ToString()
+        {
+            return name;
+        }
+    }
+    class Triangle : Shape
+    {
+        public double angle;
+        public int edge1, edge2;
+
+        public Triangle(Position position, int edge1, int edge2, double angle) : base(position, "Треугольник")
+        {
+            this.edge1 = edge1;
+            this.edge2 = edge2;
+            this.angle = angle;
+            edgeCount = 3;
+        }
+        public override double Area()
+        {
+            return (edge1*edge2*Math.Sin(angle * Math.PI / 180))/2;
+        }
+
+        public new string ToString()
+        {
+            return name;
+        }
+    }
+
 }
